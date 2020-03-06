532aa85951fb7821c9cb2d6cdb6cc65bacc74bd6
Jacqui Nosal <jacqueline.dchow@gmail.com>
2020-03-06 11:22:58 -0500

    Updates

---
+++ precache-manifest.84e58d601c5241e34b480f3609f41b01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa439f458e01d40a50d645ef55a21ef0",
+    "url": "/project-6-lit-tracker/index.html"
+  },
+  {
+    "revision": "34a29cf040ca7513c1a5",
+    "url": "/project-6-lit-tracker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dcfd69e7fa6bf487fa6c",
+    "url": "/project-6-lit-tracker/static/js/2.0d5d5d0f.chunk.js"
+  },
+  {
+    "revision": "531c09388ba0a8050c8cc035e47556f2",
+    "url": "/project-6-lit-tracker/static/js/2.0d5d5d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a29cf040ca7513c1a5",
+    "url": "/project-6-lit-tracker/static/js/main.f3b329d4.chunk.js"
+  },
+  {
+    "revision": "8c2b69fe48c216cdb8ce",
+    "url": "/project-6-lit-tracker/static/js/runtime-main.d22677d5.js"
+  }
+]);
