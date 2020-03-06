35b0a6955b8736bc36f4d747141c953c215bed58
Jacqui Nosal <jacqueline.dchow@gmail.com>
2020-03-06 09:39:15 -0500

    with axios code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
-
-// Custom Imports Here:
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Main />
-        <Footer />
+        <p>...</p>
       </div>
     );
   }
 
+  componentDidMount() {
+    axios ({
+      url: 'https://proxy.hackeryou.com',
+      method: 'GET',
+      responseType: 'json',
+      paramsSerializer: function (params) {
+        return params
+    },
+      params: {
+        method: 'GET',
+        reqUrl: "https://www.goodreads.com/search/index.xml",
+        key: '14csXzY0xdicnCrXfQSO1w',
+      },
+      xmlToJSON: false
+    }).then((res) => {      
+      console.log(res);
+    })
+  }
 }
 
 export default App;
