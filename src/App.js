7be1b6cdfbdd68a17adcab3516ee386229180314
Afua <afrimpong.15@gmail.com>
2020-03-05 19:57:25 -0500

    CALLED API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import axios from 'axios';
+import Qs from 'qs';
 // Custom Imports Here:
 
+
+
 class App extends Component {
+  
+  componentDidMount(){
+    
+
+    axios ({
+      url: 'https://proxy.hackeryou.com',
+
+      responseType: '',
+
+      paramsSerializer: function (params) {
+        return Qs.stringify(params, {arrayFormat: 'brackets'})
+      },
+
+      params: {
+        reqUrl: "https://www.goodreads.com/search/index.xml",
+        
+        params : {
+          key: '14csXzY0xdicnCrXfQSO1w',
+          method: "search_index",
+          q: "Harry Potter"
+        },
+        
+        proxyHeaders: {
+          'Access-Control-Allow-Origin': "https://proxy.hackeryou.com"
+        }
+       
+      },
+
+      xmlToJSON: false
+    }).then((res) => {
+
+      console.log(res);
+    })
+  }
+
+  // paramsSerializer allows us to pass query params into axios call
+
+
 
   render() {
+    
     return (
       <div className="App">
         <h1>Testing</h1>
@@ -17,3 +59,6 @@ class App extends Component {
   
 
 export default App;
+
+
+// possibly try building a function that converts xml to json
